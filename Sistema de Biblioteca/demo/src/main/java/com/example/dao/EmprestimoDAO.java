44f0a68cefde70fbb024fbb801dd7210dacc5442
maria-larissaO <marialarissa1415@gmail.com>
2025-02-13 13:39:37 -0300

    alterando arquivo

--- Sistema de Biblioteca/demo/src/main/java/com/example/dao/EmprestimoDAO.java
+++ Sistema de Biblioteca/demo/src/main/java/com/example/dao/EmprestimoDAO.java
@@ -22,27 +22,3 @@ public class EmprestimoDAO {
     }
 }
 
-// package com.example.dao;
-
-// import com.example.dominio.*;
-// import java.sql.*;
-
-// public class EmprestimoDAO {
-//     private Connection connection;
-
-//     public EmprestimoDAO(Connection connection) {
-//         this.connection = connection;
-//     }
-
-//     public void adicionarEmprestimo(Emprestimo emprestimo) throws SQLException {
-//         String sql = "INSERT INTO emprestimos (livro_id, usuario_id, data_emprestimo, data_devolucao) VALUES (?, ?, ?, ?)";
-//         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-//             stmt.setInt(1, emprestimo.getLivro().getId()); // Supondo que você tenha um método para obter o ID do livro
-//             stmt.setInt(2, emprestimo.getUsuario().getId()); // Supondo que você tenha um método para obter o ID do usuário
-//             stmt.setDate(3, new java.sql.Date(emprestimo.getDataEmprestimo().getTime()));
-//             stmt.setDate(4, new java.sql.Date(emprestimo.getDataDevolucao().getTime()));
-//             stmt.executeUpdate();
-//         }
-//     }
-// }
-
